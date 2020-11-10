abff5a33d9e596d4af010e1cf9362b38d68b2726
DESKTOP-QCPQCS6\ASUS <rozitakarimi65@gmail.com>
2020-11-10 04:15:43 -0800

    change message

--- learningWithRos/learningWithRos/Program.cs
+++ learningWithRos/learningWithRos/Program.cs
@@ -6,7 +6,7 @@ namespace learningWithRos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World rozita!");
         }
     }
 }
