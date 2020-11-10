47f9927f44b071f7e408a3eecf59305339466ba9
Shelly <shelly.ka1987@gmail.com>
2020-11-10 19:45:55 +1100

    for start

---
+++ learningWithRos/learningWithRos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learningWithRos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
